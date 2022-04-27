494f34776896dec8e7c2c075d369c80b6f05e462
Eli-Meitner <85237294+Eli-Meitner@users.noreply.github.com>
2022-04-27 10:04:22 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Terraform Provider: Custom resources
+
+## Provider objective
+
+This provider is intended to support customized resources that are not Terraform out of the box.
---
+++ internal/provider/resource_invoke_lambda.go
@@ -0,0 +1,138 @@
+package provider
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/lambda"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceInvokeLambda() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceInvokeLambdaCreate,
+		Read:   resourceInvokeLambdaRead,
+		Update: resourceInvokeLambdaUpdate,
+		Delete: resourceInvokeLambdaDelete,
+
+		Schema: map[string]*schema.Schema{
+			"region": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"aws_profile": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"function_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"payload": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+type getItemsRequest struct {
+	SortBy     string
+	SortOrder  string
+	ItemsToGet int
+}
+
+type getItemsResponseError struct {
+	Message string `json:"message"`
+}
+
+type getItemsResponseData struct {
+	Item string `json:"item"`
+}
+
+type getItemsResponseBody struct {
+	Result string                 `json:"result"`
+	Data   []getItemsResponseData `json:"data"`
+	Error  getItemsResponseError  `json:"error"`
+}
+
+type getItemsResponseHeaders struct {
+	ContentType string `json:"Content-Type"`
+}
+
+type getItemsResponse struct {
+	StatusCode int                     `json:"statusCode"`
+	Headers    getItemsResponseHeaders `json:"headers"`
+	Body       getItemsResponseBody    `json:"body"`
+}
+
+func invokeLambda(d *schema.ResourceData, m interface{}, action string) string {
+	var profile = d.Get("aws_profile").(string)
+	if profile == "" {
+		profile = "default"
+	}
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+		Profile:           profile,
+	}))
+	client := lambda.New(sess, &aws.Config{Region: aws.String(d.Get("region").(string))})
+
+	var j map[string]interface{}
+	var p = []byte(d.Get("payload").(string))
+	err := json.Unmarshal(p, &j)
+	j["action"] = action
+	payload, err := json.Marshal(j)
+	if err != nil {
+		return fmt.Sprintf("Error getting items, StatusCode: ", err)
+	}
+	result, err := client.Invoke(&lambda.InvokeInput{FunctionName: aws.String(d.Get("function_name").(string)), Payload: payload})
+	if err != nil {
+		return fmt.Sprintf("Error calling lambda function", err)
+	}
+	var resp getItemsResponse
+	err = json.Unmarshal(result.Payload, &resp)
+	if err != nil {
+		return fmt.Sprintf("Error unmarshalling response", err)
+	}
+
+	if resp.StatusCode != 200 {
+		fmt.Println("Error getting items, StatusCode: " + strconv.Itoa(resp.StatusCode))
+	}
+
+	if resp.Body.Result == "failure" {
+		return fmt.Sprintf("Failed to get items", err)
+	}
+
+	if len(resp.Body.Data) > 0 {
+		for i := range resp.Body.Data {
+			fmt.Println(resp.Body.Data[i].Item)
+		}
+	} else {
+		fmt.Println("There were no items")
+	}
+	str := strconv.Itoa(resp.StatusCode)
+	return str
+}
+
+func resourceInvokeLambdaCreate(d *schema.ResourceData, m interface{}) error {
+
+	d.SetId(invokeLambda(d, m, "apply"))
+	return resourceInvokeLambdaRead(d, m)
+}
+
+func resourceInvokeLambdaRead(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func resourceInvokeLambdaUpdate(d *schema.ResourceData, m interface{}) error {
+	d.SetId(invokeLambda(d, m, "apply"))
+	return resourceInvokeLambdaRead(d, m)
+}
+
+func resourceInvokeLambdaDelete(d *schema.ResourceData, m interface{}) error {
+	d.SetId(invokeLambda(d, m, "destroy"))
+	return resourceInvokeLambdaRead(d, m)
+}
